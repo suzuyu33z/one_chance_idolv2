55a447d33e49888220cbed33e810b3703f230aca
kuga7 <kcougar.te0@gmail.com>
2024-08-13 11:40:27 +0900

    commit-(subpage)

--- frontend/src/app/new/page.jsx
+++ frontend/src/app/new/page.jsx
@@ -2,23 +2,28 @@ import Link from "next/link";
 
 export default function New() {
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">まずは登録しよう！</h1>
-      <Link
-        href="/new/owner"
-        className="block text-blue-500 hover:text-blue-700"
-      >
-        ワンちゃん飼ってる人！
-      </Link>
-      <Link
-        href="/new/visitor"
-        className="block text-blue-500 hover:text-blue-700"
-      >
-        ワンちゃん飼っていない人！
-      </Link>
-      <Link href="/" className="block text-blue-500 hover:text-blue-700">
-        戻る
-      </Link>
+    <div className="max-w-lg mx-auto bg-white p-8 rounded-lg shadow-md mt-10">
+      <h1 className="text-2xl font-bold mb-8 text-center text-[#A8D38D]">
+        さあ、始めましょう
+      </h1>
+      <div className="grid gap-6">
+        <Link href="/new/owner">
+          <div className="w-full bg-[#A8D38D] text-white py-4 rounded-lg font-semibold text-center hover:bg-[#96c781] transition-colors cursor-pointer shadow-md">
+            私のわんちゃんをみんなに紹介
+          </div>
+        </Link>
+        <Link href="/new/visitor">
+          <div className="w-full bg-[#A8D38D] text-white py-4 rounded-lg font-semibold text-center hover:bg-[#96c781] transition-colors cursor-pointer shadow-md">
+            わんちゃんたちと触れ合いたい
+          </div>
+        </Link>
+        <Link href="/">
+          <div className="w-full bg-gray-300 text-gray-700 py-4 rounded-lg font-semibold text-center hover:bg-gray-400 transition-colors cursor-pointer shadow-md">
+            戻る
+          </div>
+        </Link>
+      </div>
     </div>
   );
 }
+
