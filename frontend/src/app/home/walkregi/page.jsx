1060b5a8b92aa32c74af70c57baaeaf18f6156f8
suzuki <suzuyu33z@gmail.com>
2024-08-14 22:24:52 +0900

    Add frontend files

--- frontend/src/app/home/walkregi/page.jsx
+++ frontend/src/app/home/walkregi/page.jsx
@@ -1,12 +1,203 @@
-import Link from "next/link";
+"use client";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default function WalkRegi() {
+  const [formData, setFormData] = useState({
+    description: "",
+    time_start: null,
+    time_end: null,
+    location_id: "",
+    dogs: [],
+  });
+  const [locations, setLocations] = useState([]);
+  const [userDogs, setUserDogs] = useState(null); // 初期値をnullに設定
+  const [userInfo, setUserInfo] = useState(null); // ユーザー情報を格納
+  const router = useRouter();
+
+  useEffect(() => {
+    // ユーザー情報を取得
+    fetch(`${process.env.API_ENDPOINT}/api/user-info`, {
+      method: "GET",
+      credentials: "include",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setUserInfo(data);
+        if (data.dog_number === 0) {
+          return; // 犬が0匹の場合、他のデータを取得しない
+        }
+        // ロケーションリストを取得
+        fetch(`${process.env.API_ENDPOINT}/api/locations`)
+          .then((response) => response.json())
+          .then((data) => setLocations(data))
+          .catch((error) => console.error("Error fetching locations:", error));
+
+        // ユーザーの犬リストを取得
+        fetch(`${process.env.API_ENDPOINT}/api/user-dogs`, {
+          method: "GET",
+          credentials: "include",
+        })
+          .then((response) => response.json())
+          .then((data) => setUserDogs(data))
+          .catch((error) => console.error("Error fetching user dogs:", error));
+      })
+      .catch((error) => console.error("Error fetching user info:", error));
+  }, []);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleDateChange = (name, date) => {
+    setFormData({
+      ...formData,
+      [name]: date,
+    });
+  };
+
+  const handleDogSelect = (e) => {
+    const dogId = e.target.value;
+    const isChecked = e.target.checked;
+    setFormData((prevState) => {
+      const updatedDogs = isChecked
+        ? [...prevState.dogs, dogId]
+        : prevState.dogs.filter((id) => id !== dogId);
+      return {
+        ...prevState,
+        dogs: updatedDogs,
+      };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(`${process.env.API_ENDPOINT}/api/register-walk`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include", // セッションを含める
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        if (response.ok) {
+          router.push("/home");
+        } else {
+          throw new Error("Failed to register walk.");
+        }
+      })
+      .catch((error) => console.error("Error registering walk:", error));
+  };
+
+  if (userInfo === null) {
+    // userInfoが取得されるまで待つ
+    return <div>Loading...</div>;
+  }
+
+  if (userInfo.dog_number === 0) {
+    // 犬の数が0の場合
+    return <div>権限がありません</div>;
+  }
+
+  if (userDogs === null) {
+    // userDogsが取得されるまで待つ
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">新たなさんぽを登録！</h1>
-      <Link href="/home" className="text-blue-500 hover:text-blue-700">
-        戻る
-      </Link>
+      <h1 className="text-2xl font-bold mb-4">新たな散歩を登録！</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-semibold mb-2">
+            開始時間
+          </label>
+          <DatePicker
+            selected={formData.time_start}
+            onChange={(date) => handleDateChange("time_start", date)}
+            showTimeSelect
+            timeFormat="HH:mm"
+            timeIntervals={15}
+            dateFormat="yyyy/MM/dd HH:mm"
+            className="w-full border border-gray-300 p-3 rounded-lg focus:outline-none"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-semibold mb-2">
+            終了時間
+          </label>
+          <DatePicker
+            selected={formData.time_end}
+            onChange={(date) => handleDateChange("time_end", date)}
+            showTimeSelect
+            timeFormat="HH:mm"
+            timeIntervals={15}
+            dateFormat="yyyy/MM/dd HH:mm"
+            className="w-full border border-gray-300 p-3 rounded-lg focus:outline-none"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-semibold mb-2">
+            ロケーション
+          </label>
+          <select
+            name="location_id"
+            value={formData.location_id}
+            onChange={handleChange}
+            className="w-full border border-gray-300 p-3 rounded-lg focus:outline-none"
+            required
+          >
+            <option value="">ロケーションを選択</option>
+            {locations.map((location) => (
+              <option key={location.location_id} value={location.location_id}>
+                {location.location_name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-semibold mb-2">
+            犬を選択
+          </label>
+          {userDogs.map((dog) => (
+            <div key={dog.dog_id} className="mb-2">
+              <input
+                type="checkbox"
+                name="dogs"
+                value={dog.dog_id}
+                onChange={handleDogSelect}
+              />{" "}
+              {dog.dog_name}
+            </div>
+          ))}
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-semibold mb-2">
+            コメント
+          </label>
+          <textarea
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            className="w-full border border-gray-300 p-3 rounded-lg focus:outline-none"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-blue-500 text-white p-3 rounded-lg font-semibold hover:bg-blue-700 transition-colors"
+        >
+          登録
+        </button>
+      </form>
     </div>
   );
 }
--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,16 +1,59 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 import "./globals.css";
 
 export default function Top() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // APIエンドポイントにPOSTリクエストを送信
+    const response = await fetch(process.env.API_ENDPOINT + `/api/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }), // フォームデータをJSONに変換して送信
+      credentials: "include", // <-- これが重要
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      // ホームページへリダイレクト
+      router.push("/home");
+    } else {
+      console.error("Login failed.");
+      // ログイン失敗時のエラーハンドリング
+    }
+  };
+
   return (
     <div className="flex flex-col min-h-screen bg-gradient-to-b from-white to-gray-100">
       {/* ヘッダー部分 */}
-      <header className={`bg-white py-0 px-2 border-b-2 border-[#c5e1a5] text-center fixed top-0 w-full z-[1000] flex justify-between items-center duration-100`}>
+      <header
+        className={`bg-white py-0 px-2 border-b-2 border-[#c5e1a5] text-center fixed top-0 w-full z-[1000] flex justify-between items-center duration-100`}
+      >
         <svg width="220" height="40" viewBox="0 10 220 40">
           <defs>
             <path id="curve" d="M 10,40 Q 100,20 190,40" />
           </defs>
-          <text width="220" style={{ fontFamily: 'Kosugi Maru, sans-serif', fill: '#75A05A', fontSize: '14px', fontWeight: 'bold', letterSpacing: '-0.5px', textDecoration: 'underline' }}>
+          <text
+            width="220"
+            style={{
+              fontFamily: "Kosugi Maru, sans-serif",
+              fill: "#75A05A",
+              fontSize: "14px",
+              fontWeight: "bold",
+              letterSpacing: "-0.5px",
+              textDecoration: "underline",
+            }}
+          >
             <textPath xlinkHref="#curve" dy={-5}>
               わん^Chance^アイドル
             </textPath>
@@ -23,19 +66,52 @@ export default function Top() {
         <h1 className="text-2xl font-bold mb-6 text-gray-800">
           わんちゃんと、特別な体験を。
         </h1>
-        <div className="flex flex-col items-center w-full space-y-6">
-          <Link
-            href="/login"
-            className="w-2/3 max-w-xs py-4 text-center rounded-full text-white bg-[#A8D38D] hover:bg-[#96c781] transition-colors shadow-lg transform hover:scale-105"
-          >
-            今すぐ参加
-          </Link>
-          <Link
-            href="/new"
-            className="w-2/3 max-w-xs py-4 text-center rounded-full text-[#A8D38D] bg-white border border-[#A8D38D] hover:bg-gray-50 transition-colors shadow-lg transform hover:scale-105"
-          >
-            新規登録はこちら
-          </Link>
+        <div className="w-full max-w-md p-8 bg-white shadow-xl rounded-lg">
+          <h1 className="text-2xl font-bold text-gray-800 mb-8 text-center">
+            ようこそ！
+          </h1>
+          <p className="text-lg text-gray-600 mb-6 text-center">
+            特別な体験を始めましょう。
+          </p>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-6">
+              <label className="block text-gray-700 font-medium mb-2">
+                メールアドレス
+              </label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-full border border-gray-300 p-3 rounded focus:outline-none focus:ring-2 focus:ring-[#A8D38D] focus:border-transparent"
+                placeholder="例: user@example.com"
+                required
+              />
+            </div>
+            <div className="mb-6">
+              <label className="block text-gray-700 font-medium mb-2">
+                パスワード
+              </label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full border border-gray-300 p-3 rounded focus:outline-none focus:ring-2 focus:ring-[#A8D38D] focus:border-transparent"
+                placeholder="例: ********"
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-[#A8D38D] text-white py-3 rounded-lg font-semibold hover:bg-[#96c781] transition-colors shadow-md"
+            >
+              ログイン
+            </button>
+          </form>
+          <div className="mt-6 text-center">
+            <Link href="/new" className="text-[#A8D38D] hover:underline">
+              新規登録はこちら
+            </Link>
+          </div>
         </div>
       </main>
 
@@ -46,5 +122,3 @@ export default function Top() {
     </div>
   );
 }
-
-
